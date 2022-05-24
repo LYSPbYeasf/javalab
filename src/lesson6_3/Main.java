d0ed8d915422d68722055c2affd669fffb0e7f4d
LYSPbYeasf <sloshadka@mail.ru>
2022-05-24 22:04:32 +0300

    Lab6-3

--- src/lesson6_3/Main.java
+++ src/lesson6_3/Main.java
@@ -2,7 +2,7 @@ package lesson6_3;
 
 public class Main {
     public static void main(String[] args) {
-       Truck truck = new Truck(6000, "KAMAZ", 'G', 110, 8, 9000);
+       Truck truck = new Truck(6000, "Камаз", 'G', 110, 8, 9000);
        truck.outPut();
        truck.newWheels(10);
        truck.outPut();
