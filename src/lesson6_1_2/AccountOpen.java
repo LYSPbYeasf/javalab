906528d458daf8f2161abbb0a84e896661143e4e
LYSPbYeasf <sloshadka@mail.ru>
2022-05-26 13:31:38 +0300

    Lab6

--- src/lesson6_1_2/AccountOpen.java
+++
@@ -1,7 +0,0 @@
-package lesson6_1_2;
-
-public interface AccountOpen {
-
-    void openBankAccount();
-    int creditSum();
-}
--- src/lesson6_4/Child.java
+++
@@ -1,9 +0,0 @@
-package lesson6_4;
-
-import java.util.Scanner;
-
-public class Child extends Parent {
-    public void printNumber() {
-        System.out.println("Число равно: " + getNumber());
-    }
-}
